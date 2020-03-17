99fd2bc99ca48599141c78edf997b2c4aa7a0a02
arichards4814 <arichards4814@gmail.com>
2020-03-17 15:21:29 -0400

    added material ui and created react app

---
+++ boardgame_frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Button from '@material-ui/core/Button';
+
+function App() {
+  return (
+    <div>
+      <Button> Hello World </Button>
+    </div>
+  );
+}
+
+export default App;
